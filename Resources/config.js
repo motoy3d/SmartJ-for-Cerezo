a32b470f6a2634ed5dc6600e1060d7f8a41aefba
motoy3d <motoy3d@gmail.com>
2014-12-15 04:06:52 +0900

    first commit

---
+++ Resources/config.js
@@ -0,0 +1,27 @@
+exports.config = {
+    teamId: 'cerezo'
+    ,hashtag: 'cerezo'
+    ,teamName: 'C大阪'
+    ,appName: 'スマートJ for セレッソ大阪'
+    ,iPhoneAppUrl: 'https://itunes.apple.com/jp/app/id929041389?l=ja&ls=1&mt=8'
+    ,androidAppUrl: 'https://play.google.com/store/apps/details?id=smartj.cerezo'
+    ,color: '#e72b8c'
+    ,tintColor: 'white'
+    ,dbName: 'smartj.cerezo'
+    //ニュースのソースURL
+    ,feedUrlBase: 'http://sub0000499082.hmk-temp.com/redsmylife/news.json'
+    //日程・結果のソースURL
+    ,resultsUrl: "http://sub0000499082.hmk-temp.com/redsmylife/results.json?teamId=cerezo&season="
+    //設定、メッセージのソースURL
+    ,messageUrl: "http://sub0000499082.hmk-temp.com/redsmylife/config.json?teamId=cerezo"
+    ,newsEntriesPerPage: 50
+    ,googleAnalyticsTrackingId: 'UA-30928840-1'
+    //nend(iPhone)
+    ,nendSpotIdIPhoneIcon: 248613
+    ,nendApiKeyIPhoneIcon: 'd07a64e174c6443935e48320082206408df7e021'
+    ,nendSpotIdIPhoneBanner: 248612
+    ,nendApiKeyIPhoneBanner: '0e0d5dceabd53de6d7b247f1669aaa0b79585364'
+    //nend(Android)
+    ,nendSpotIdAndroid: 248614
+    ,nendApiKeyAndroid: 'e2d35ca49a65c9638c7de152d7696888a95ce708'
+};
