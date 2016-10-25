bac6e3fe2bb8fdf21471a9282f5399c8e2cc0f83
motoy3d <motoy3d@gmail.com>
2016-10-25 22:50:37 +0900

    ver1.0.3

--- Resources/config.js
+++ Resources/config.js
@@ -2,18 +2,40 @@ exports.config = {
     teamId: 'cerezo'
     ,hashtag: 'cerezo'
     ,teamName: 'C大阪'
+    ,teamNameFull: 'セレッソ大阪'
     ,appName: 'スマートJ for セレッソ大阪'
     ,iPhoneAppUrl: 'https://itunes.apple.com/jp/app/id929041389?l=ja&ls=1&mt=8'
     ,androidAppUrl: 'https://play.google.com/store/apps/details?id=smartj.cerezo'
+    ,backgroundColor: 'black'
+	,visitedBgColor : '#457'
+    ,mainTextColor: 'white'
     ,color: '#e72b8c'
     ,tintColor: 'white'
+    ,standingsBgcolor: '#e72b8c'
+    ,standingsTeamFontColor: 'white'
+    ,resultsDetailBtnBgColorActive: '#333'
+    ,resultsDetailBtnColorActive: 'white'
+    ,resultsDetailBtnBgColorInactive: '#202020'
+    ,resultsDetailBtnColorInactive: '#444'
     ,dbName: 'smartj.cerezo'
+    //URLベース
+    ,urlBase: 'https://smartj.mobi/redsmylife/'
     //ニュースのソースURL
     ,feedUrlBase: 'http://sub0000499082.hmk-temp.com/redsmylife/news.json'
     //日程・結果のソースURL
     ,resultsUrl: "http://sub0000499082.hmk-temp.com/redsmylife/results.json?teamId=cerezo&season="
+    //順位表のソースURL
+    ,standingsUrl: 'http://sub0000499082.hmk-temp.com/redsmylife/standings.json'
     //設定、メッセージのソースURL
     ,messageUrl: "http://sub0000499082.hmk-temp.com/redsmylife/config.json?teamId=cerezo"
+    //動画のソースURL
+    ,videoUrl: "http://sub0000499082.hmk-temp.com/redsmylife/video.json?teamId=cerezo"
+    //報告URL
+    ,reportUrl: "https://smartj.mobi/redsmylife/report.json?teamId=cerezo"
+    //利用規約URL
+    ,rulesUrl: "https://smartj.mobi/redsmylife/rules.jsp?appName="
+    //開発元メールアドレス
+    ,developerMail: "motoy3d+smartj@gmail.com"
     ,newsEntriesPerPage: 50
     ,googleAnalyticsTrackingId: 'UA-30928840-1'
     //nend(iPhone)
@@ -24,4 +46,6 @@ exports.config = {
     //nend(Android)
     ,nendSpotIdAndroid: 248614
     ,nendApiKeyAndroid: 'e2d35ca49a65c9638c7de152d7696888a95ce708'
+    ,nendSpotIdAndroidBanner: 393952
+    ,nendApiKeyAndroidBanner: '2d837f2de11a257805b7750512dff6c2b8cc93b8'
 };
